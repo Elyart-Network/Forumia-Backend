7b2c6347d2bb0d62582090570698b735260745b4
BaiYi <me@baiyi.icu>
2022-10-19 16:37:15 +0800

    Add dependencies for storage

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     kotlin("plugin.serialization")
 }
 
-
 dependencies {
+    api(project(":storage"))
     api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.0")
     api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
     api("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.4.0")
