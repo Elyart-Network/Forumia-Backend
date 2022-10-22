2af7186fc3a04ed5b9b67ee650b60b41f2f82951
BaiYi <me@baiyi.icu>
2022-10-22 17:07:23 +0800

    Update sysgetenv.kt
    FORUMIA-T-4, FORUMIA-T-5

--- storage/src/main/kotlin/net/elyow/forumia/storage/plugins/sysgetenv.kt
+++ storage/src/main/kotlin/net/elyow/forumia/storage/plugins/sysgetenv.kt
@@ -3,15 +3,12 @@ package net.elyow.forumia.storage.plugins
 import java.io.File
 
 object sysgetenv {
-    private fun isFileExists(file: File): Boolean {
-        return file.exists() && !file.isDirectory
-    }
-    private fun dot(): Map<String, String>? {
-        if(isFileExists(File(".env"))) {
-            return File(".env").readLines()
-                .filterNot { it.startsWith("#") }
-                .associate { val (k, v) = it.split("="); k to v }
-        } else return null
-    }
-    operator fun get(key: String): String = System.getenv(key) ?: (dot()?.get(key) ?: error("Can't find $key in environment variables"))
+    private val dotEnv = runCatching {
+        File(".env").readLines()
+            .filterNot { it.startsWith("#") }
+            .associate { val (k, v) = it.split("="); k to v }
+    }.getOrElse { emptyMap() }
+
+    operator fun get(key: String) = dotEnv[key] ?: System.getenv(key) ?: error("Can't find $key in environment variables")
+
 }
