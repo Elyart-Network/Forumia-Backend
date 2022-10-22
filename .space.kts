ab7f3f03205980b5828ac1c36f2e2b46ca41b11d
Season Shi <seasonshi@elyart.dev>
2022-10-22 09:59:48 +0000

    Gradle Project Test

--- .space.kts
+++ .space.kts
@@ -4,6 +4,11 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job("Build and run tests") {
+    container(displayName = "Gradle build", image = "openjdk:18") {
+        kotlinScript { api ->
+            // here goes complex logic
+            api.gradlew("build")
+        }
+    }
 }
